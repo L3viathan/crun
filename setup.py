2942900ad23a9ed679426bb38ff1ef7a02063227
L3viathan <git@l3vi.de>
2019-03-27 21:35:24 +0100

    Version bump (2.1.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crun",
-    version="2.1.0",
+    version="2.1.1",
     install_requires=[
         "click",
         "colorful",
@@ -23,6 +23,7 @@ setup(
         "console_scripts": ["crun=crun.runner:cli"],
     },
 )
+
 
 
 
